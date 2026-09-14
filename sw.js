adf237c875620412fce432084615b8baab24e13d
الباشمحاسب pdf&calc <evraimnesim999@gmail.com>
2026-09-14 03:07:45 +0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,13 +1,15 @@
-const CACHE_NAME = "elbash-mohaseb-v3";
+const CACHE_NAME = "elbash-mohaseb-v1";
 
 const FILES_TO_CACHE = [
   "./",
   "./index.html",
-  "./shopcalc.html"
+  "./shopcalc.html",
+  "./manifest.json",
+  "./icon-192.png",
+  "./icon-512.png"
 ];
 
 self.addEventListener("install", event => {
-
   event.waitUntil(
     caches.open(CACHE_NAME).then(cache => {
       return cache.addAll(FILES_TO_CACHE);
@@ -15,41 +17,26 @@ self.addEventListener("install", event => {
   );
 
   self.skipWaiting();
-
 });
 
 self.addEventListener("activate", event => {
-
   event.waitUntil(
-
     caches.keys().then(keys => {
-
       return Promise.all(
-
         keys
           .filter(key => key !== CACHE_NAME)
           .map(key => caches.delete(key))
-
       );
-
     })
-
   );
 
   self.clients.claim();
-
 });
 
 self.addEventListener("fetch", event => {
-
   event.respondWith(
-
     caches.match(event.request).then(cachedResponse => {
-
       return cachedResponse || fetch(event.request);
-
     })
-
   );
-
 });
